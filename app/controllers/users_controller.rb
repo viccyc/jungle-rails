e9f2223705efe0ec1beb61e5e8288bf3e59d350f
Viccy Grace <viccy.c.grace@gmail.com>
2018-03-02 16:45:38 +0000

    empty cart message and link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    puts "User controller user: #{@user}"
     if user.save!
       session[:user_id] = user.id
       redirect_to '/'
